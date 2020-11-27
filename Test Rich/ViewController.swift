15b6f356513c7d0975460eabde1dc4cba267a843
Patrik Hauguth <patrik.hauguth@gmail.com>
2020-11-27 21:19:23 +0100

    first app

---
+++ Test Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Rich
+//
+//  Created by Patrik Hauguth on 2020-11-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
